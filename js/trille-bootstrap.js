88f40918beb34eba0d421c5e10908616ef18e843
summour <144230741+summour@users.noreply.github.com>
2026-05-03 00:11:23 +0700

    Load compact bar stylesheet directly

--- js/trille-bootstrap.js
+++ js/trille-bootstrap.js
@@ -1,1 +1,11 @@
+function loadCompactBarsStyles(){
+  if(document.querySelector('link[data-trille-compact-bars]'))return;
+  const link=document.createElement('link');
+  link.rel='stylesheet';
+  link.href='trille-compact-bars.css';
+  link.dataset.trilleCompactBars='true';
+  document.head.appendChild(link);
+}
+
+loadCompactBarsStyles();
 init();
